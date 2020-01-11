6793c878fec3fd8ec4ecb095833a739660dbd930
Aurelie Verrot <aurelie+github@verrot.fr>
2020-01-11 10:40:28 -0800

    tip calculator: working version

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+let result = 0;
+
 // Calculate tip - main function
 function calculateTip() {
 
 	var billAmount = document.getElementById("amount").value;
-	var option = document.getElementById("option-value").value;
+	var option = document.getElementById("dropdown-options").value;
 	var nbPeople = document.getElementById("nbpeople").value;
-
 
 // alert if no input
 	if (billAmount === "" || option == 0) {
@@ -12,31 +13,26 @@ function calculateTip() {
 		return;
 	}
 
-
 // check if number of people is empty or less than one
 	if (nbPeople === "" || nbPeople <= 1) {
 		nbPeople = 1;
 
 	}
 
-
 // Calculate the tip divided by number of people
-	var result = (billAmount * option) / nbPeople;
+	result = (billAmount * option) / nbPeople;
 	result = Math.round(result * 100) / 100;
 	result = result.toFixed(2);
 	console.log(result);
 
 }
 
-// Hide the tip amount on load ?????
-
-
 
 // function call when button is clicked
 document.getElementById("calculate").onclick = function() {
 	calculateTip();
+	document.getElementById("tip-result").innerHTML = `The tip is $ ${result} per person`;
 };
 
 
 // function that let appear the option chosen
- 
